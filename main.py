f51556d5154f6098ce83faf842a0f14683a95cac
kelvinleandro <kelvinleandro5@gmail.com>
2026-02-25 18:00:57 -0300

    feat: setup project to use logs

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import logging
+import os
+from datetime import datetime
+
+os.makedirs("out", exist_ok=True)
+
+file_name = datetime.now().strftime("log_%Y-%m-%d.log")
+path_log = os.path.join("out", file_name)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler(path_log, encoding="utf-8"), logging.StreamHandler()],
+)
+
+logger = logging.getLogger(__name__)
+
+
 def main():
-    print("Hello from eeg-preprocessing!")
+    logger.info("Hello from eeg-preprocessing!")
 
 
 if __name__ == "__main__":
